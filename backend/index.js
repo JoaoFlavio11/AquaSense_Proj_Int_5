422b7b8cc7c7abfe8ea89489f823f4dbdf68cc67
JoaoFlavio11 <joaoflaviocl@gmail.com>
2024-05-07 00:14:43 -0300

    teste e link do servidor

--- backend/index.js
+++ backend/index.js
@@ -86,6 +86,7 @@ inserirDados();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`http://localhost:3000/`);
 });
 
 
