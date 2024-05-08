c2f2dd24e5fc1531f476624fd9bb69640cc5ab0b
JoaoFlavio11 <joaoflaviocl@gmail.com>
2024-05-07 21:53:47 -0300

    link no terminal

--- backend/index.js
+++ backend/index.js
@@ -88,4 +88,5 @@ inserirDados();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Clique aqui: http://localhost:${PORT}`);
 });
