ae6927ca55dc2752f6e245bb490f738a4dd2fe61
JoaoFlavio11 <joaoflaviocl@gmail.com>
2024-05-29 20:16:51 -0300

    adição adm firebase express + rota POST no servidor

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const admin = require('firebase-admin');
 
 const app = express();
 
-//arquivo JSON credenciais do Firebase Admin SDK
+// arquivo JSON credenciais do Firebase Admin SDK
 const serviceAccount = require('./firebase/monitoramento-de-tanque-347a4-firebase-adminsdk-6fubm-dcc62cd1bb.json');
 
 // Inicialize o Firebase com as credenciais
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://monitoramento-de-tanque.firebaseio.com' //URL do bd
+    databaseURL: 'https://monitoramento-de-tanque.firebaseio.com' // URL do bd
 });
 
 const db = admin.firestore();
@@ -18,6 +18,9 @@ const db = admin.firestore();
 // arquivos estáticos diretos da pasta 'frontend'
 const frontendPath = path.join(__dirname, '../frontend');
 app.use(express.static(frontendPath));
+
+// Middleware para parsear o JSON no corpo das requisições
+app.use(express.json());
 
 // Rota para pegar os dados do tanque de água
 app.get('/dados', async (req, res) => {
@@ -65,35 +68,67 @@ app.get('/dadosFiltrados', async (req, res) => {
     }
 });
 
+// Rota para gravar dados enviados pelo ESP32
+app.post('/gravarDados', async (req, res) => {
+    try {
+        const { nome, completo, funcionando, temperatura, ph } = req.body;
 
+        // Formatar a data
+        const dataAtual = new Date();
+        const formatoOriginal = dataAtual.toLocaleString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
 
-// Servir o frontend na rota raiz (`/`) => trocar para login.html
+        const formatoPersonalizado = formatoOriginal.replace(', ', ' | ');
+
+        // Referência do documento no Firestore
+        const docRef = db.collection('tanque').doc();
+
+        // Inserir um novo documento na coleção "tanque"
+        await docRef.set({
+            timestamp: formatoPersonalizado,
+            nome,
+            completo,
+            funcionando,
+            temperatura,
+            ph
+        });
+
+        res.status(201).json({ mensagem: 'Dados inseridos com sucesso!' });
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao gravar dados' });
+    }
+});
+
+// Servir o frontend na rota raiz (`/`) 
 app.get('/', (req, res) => {
     res.sendFile(path.join(frontendPath, 'html', 'login.html'));
 });
 
-//rota para recuperar senha
+// rota para recuperar senha
 app.get('/recuperarSenha', (req, res) => {
     res.sendFile(path.join(frontendPath, 'html', 'recuperarSenha.html')); 
 });
 
-//rota para a página principal
+// rota para a página principal
 app.get('/paginaInicial', (req, res) => {
     res.sendFile(path.join(frontendPath, 'html', 'index.html'));
 });
 
-//rota gráficos de temperatura
+// rota gráficos de temperatura
 app.get('/temperatura', (req, res) => {
     res.sendFile(path.join(frontendPath, 'html', 'temperatura.html'));
 });
 
-//rota para os gráficos do pH da água
+// rota para os gráficos do pH da água
 app.get('/ph', (req, res) => {
     res.sendFile(path.join(frontendPath, 'html', 'ph.html'));
 });
-//criar outras rotas
-
-
+// criar outras rotas
 
 
 // Inicie o servidor na porta 3000
